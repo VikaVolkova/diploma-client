72a75052e5e4d7ac29cc4e5c962f57dfd6928d58
Viktoriia Volkova <viktoriafeldman26@gmail.com>
2022-11-16 15:53:58 +0100

    Fix interceptor and api

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
 
 import App from "./App";
 import "./index.css";
@@ -17,3 +18,5 @@ root.render(
     </Router>
   </React.StrictMode>
 );
+
+reportWebVitals();
